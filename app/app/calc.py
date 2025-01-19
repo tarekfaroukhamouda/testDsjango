e5fc96fc820ae07d3a74461beb8b7a51e240167f
Tarek Hamouda <tarek@ensofia.com>
2025-01-19 13:55:28 +0200

    add database postgres envorments

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+'''
+calculator funcction
+'''
+
+def add(x,y):
+    '''add x and y return result'''
+    return x + y
+
+def subtract(x,y):
+
+    if x >y:
+        return x-y
+    else:
+        return y-x
--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        "ENGINE":'django.db.backends.postgresql',
+        "HOST":os.environ.get('DB_HOST'),
+        "NAME":os.environ.get('DB_NAME'),
+        "USER":os.environ.get('DB_USER'),
+        "PASSWORD":os.environ.get('DB_PASSWORD')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
